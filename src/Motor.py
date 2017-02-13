c09f34234632b5a1d23c6621f21cdb61d3088c53
Dom Bozzuto <dlbozzuto@wpi.edu>
2017-02-13 16:46:56 -0500

    development on motor implementation

--- src/Motor.py
+++ src/Motor.py
@@ -1,30 +1,53 @@
-from CanTalonSRX import *
-import can
-
 class Motor:
 
-	def __init__(self, devID, canBus):
+	def __init__(self, devID, mode):
 		self.deviceID = devID
-		self.bus = canBus
-		self.speed = 0
-		self.direction = 0
-		self.talon = CanTalonSRX(self.deviceID)
-		self.talon.set_mode(Mode.DutyCycle)
+		self.setpoint_val = 0
+		self.actual_val = 0
+		self.mode = mode
+		self.forward_limit = False
+		self.reverse_limit = False
 
-	def update(self):
+	'''
+	Takes a string representing containing motor information
+	If the device ID of this motor matches the string, update the values
+	accordingly.  If it does not match, ignore the message. Similarly, if
+	the message is invald, ignore it
+	'''
+	def update(self, update_info):
+		# remove the < and >
+		message = message[1:-1]
 
-		if not self.talon.txQueue.empty():
-			msg = talon.txQueue.get_nowait()
-			msg.timestamp = time.time()
-			self.bus.send(msg)
+		# split the message meaningful parts
+		message_parts = message.split(":")
 
-		recv = bus.recv(0.0)
-		if recv != None:
-			motor.talon.update(recv)
+		if(len(message_parts) < 2):
+			print "ERROR: Message has too few components and is probably malformed. Device ID: " + str(self.deviceID)
+			return None
+
+
+		# be ready to throw an error
+		try:
+			#try to read all the values before assigning anything
+			msg_deviceID = int(message_parts[0])
+			msg_feedback_val = int(message_parts[1])
+			msg_forward_limit = bool(int(message_parts[2]))
+			msg_reverse_limit = bool(int(message_parts[3]))
+
+			if(msg_deviceID == self.deviceID):
+				self.forward_limit = msg_forward_limit
+				self.reverse_limit = msg_reverse_limit
+				self.actual_val = msg_feedback_val
+
+		except:
+			print "ERROR: Unable to parse one or more of the message components. Device ID: " + str(self.deviceID)
 
 	#set the speed of the motor from -1.0 to 1.0
 	def setSpeed(self, speed):
-		self.speed = speed
-		self.talon.set_throttle(self.speed)
+		self.setpoint_val = speed
+
+	def formMessage(self):
+		return "<" + str(deviceID) + ":" + str(mode) + ":" + str(setpoint) + ">"
+
 
 	
--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
+import Constants as CONSTANTS
+
 from Motor import Motor
 from MotorHandler import MotorHandler
 from Sensor import Sensor
 from SensorHandler import SensorHandler
+from RobotState import RobotState
 
 # initialization:
 # CAN Bus Device IDs
-leftDriveDeviceID = 0
-rightDriveDeviceID = 1
-collectorDepthDeviceID = 2
-collectorScoopsDeviceID = 3
-winchDeviceID = 4
+leftDriveDeviceID = 1
+rightDriveDeviceID = 2
+collectorDepthDeviceID = 3
+collectorScoopsDeviceID = 4
+winchDeviceID = 5
 
 #initialize handlers
 motorHandler = MotorHandler()
@@ -47,3 +50,100 @@ sensorHandler.addSensor(collectorScoopsCurrentSense)
 sensorHandler.addSensor(winchMotorCurrentSense)
 sensorHandler.addSensor(scoopReedSwitch)
 sensorHandler.addSensor(bucketMaterialDepthSense)
+
+# initialize robotState
+RobotState robotState = RobotState()
+
+
+
+# final line before entering main loop
+robotEnabled = True
+
+while robotEnabled:
+
+	currentState = robotState.getState()
+	lastState = robotState.getLastState()
+
+	# +----------------------------------------------+
+	# |              Current State Logic             |
+	# +----------------------------------------------+
+	# State machine handles the robot's current states
+	
+	if(currentState == "OFF"):
+		leftDriveMotor.setSpeed(0)
+		rightDriveMotor.setSpeed(0)
+		collectorDepthMotor.setSpeed(0)
+		collectorScoopsMotor.setSpeed(0)
+		winchMotor.setSpeed(0)
+
+	else if(currentState == "ROTATE_TO_MARKER"):
+		#rotate motors according to camera
+
+	else if(currentState == "DRIVE_TO_DIG_AREA"):
+		leftDriveMotor.setSpeed(CONSTANTS.DRIVE_SPEED)
+		rightDriveMotor.setSpeed(-CONSTANTS.DRIVE_SPEED)
+		collectorDepthMotor.setSpeed(0)
+		collectorScoopsMotor.setSpeed(0)
+		winchMotor.setSpeed(0)
+
+	else if(currentState == "STOP_AT_DIG_AREA"):
+		leftDriveMotor.setSpeed(0)
+		rightDriveMotor.setSpeed(0)
+		collectorDepthMotor.setSpeed(0)
+		collectorScoopsMotor.setSpeed(0)
+		winchMotor.setSpeed(0)
+
+	else if(currentState == "RUN_SCOOPS"):
+		leftDriveMotor.setSpeed(0)
+		rightDriveMotor.setSpeed(0)
+		collectorScoopsMotor.setSpeed(CONSTANTS.SCOOP_SPEED)
+		collectorDepthMotor.setSpeed(0)
+		winchMotor.setSpeed(0)
+
+	else if(currentState == "INCREMENT_DEPTH"):
+		collectorDepthMotor.setSpeed(CONSTANTS.DEPTH_SPEED)
+
+	else if(currentState == "DECREMENT_DEPTH"):
+		collectorDepthMotor.setSpeed(-CONSTANTS.DEPTH_SPEED)
+
+	else if(currentState == "STOP_SCOOPS"):
+		collectorScoopsMotor.setSpeed(0)
+
+	else if(currentState == "DRIVE_TO_COLLECTION_BIN"):
+		leftDriveMotor.setSpeed(-CONSTANTS.DRIVE_SPEED)
+		rightDriveMotor.setSpeed(CONSTANTS.DRIVE_SPEED)
+
+	else if(currentState == "STOP_AT_COLLECTION_BIN"):
+		leftDriveMotor.setSpeed(0)
+		rightDriveMotor.setSpeed(0)
+
+	else if(currentState == "RAISE_BUCKET"):
+		winchMotor.setSpeed(CONSTANTS.WINCH_SPEED)
+
+	else if(currentState == "LOWER_BUCKET"):
+		winchMotor.setSpeed(-CONSTANTS.WINCH_SPEED)
+
+	else if(currentState == "TELEOP_CONTROL"):
+		#do teleop
+		pass
+
+	else: # error state, set errything to off
+		leftDriveMotor.setSpeed(0)
+		rightDriveMotor.setSpeed(0)
+		collectorDepthMotor.setSpeed(0)
+		collectorScoopsMotor.setSpeed(0)
+		winchMotor.setSpeed(0)
+
+	# +----------------------------------------------+
+	# |               Next State Logic               |
+	# +----------------------------------------------+
+	# Conditionals to determine the next state
+	if(currentState == "OFF"):
+		
+		# if last state was 'None', robot is just starting
+		if(lastState == None):
+			robotState.setState("ROTATE_TO_MARKER");
+
+	else if(currentState == "ROTATE_TO_MARKER"):
+
+		# if marker is found, rotate towards dig area
