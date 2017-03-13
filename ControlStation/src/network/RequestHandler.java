ba69280febf2d9f77eb48bc81f83f91c984790e6
Dom Bozzuto <dlbozzuto@wpi.edu>
2017-03-12 23:01:14 -0400

    robot capable of interpreting messages from control station

--- ControlStation/src/network/RequestHandler.java
+++ ControlStation/src/network/RequestHandler.java
@@ -21,18 +21,19 @@ class RequestHandler extends Thread
             PrintWriter out = new PrintWriter( socket.getOutputStream() );
 
             // Write out our header to the client
-            out.println( "Echo Server 1.0" );
-            out.flush();
+            //out.println( "Echo Server 1.0" );
+            //out.flush();
 
             // Echo lines back to the client until the client closes the connection or we receive an empty line
-            String line = in.readLine();
-            while( line != null && line.length() > 0 )
-            {
-                out.println( "Echo: " + line );
-                out.flush();
-                line = in.readLine();
-            }
-
+            //String line = in.readLine();
+            //while( line != null && line.length() > 0 )
+            //{
+            //    out.println( "Echo: " + line );
+            //    out.flush();
+            //    line = in.readLine();
+            //}
+            out.println("<0|0>");
+            out.flush();
             // Close our connection
             in.close();
             out.close();
